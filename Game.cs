f8e5c828df10fff718c7f7f1c063b10382e0bfcb
vincent yap <lastency@gmail.com>
2024-12-21 15:16:31 +0800

    project initialized

---
+++ Game.cs
@@ -0,0 +1,10 @@
+﻿namespace RogueSharpTutorial
+{
+    internal class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
