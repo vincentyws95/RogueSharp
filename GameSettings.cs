49e436e59fadcfac91c9da9c1cb2ea05088ec2cc
vincent yap <lastency@gmail.com>
2024-12-21 16:57:04 +0800

    use sadconsole,  re initialized

---
+++ GameSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueSharpTutorial
+{
+    static class GameSettings
+    {
+        public const int GAME_WIDTH = 100;
+        public const int GAME_HEIGHT = 50;
+    }
+}
---
+++ MyGame.cs
@@ -0,0 +1,24 @@
+﻿namespace RogueSharpTutorial
+{
+    public class MyGame
+    {
+
+        public static void Main()
+        {
+
+        Settings.WindowTitle = "RogueSharp Tutorial - Level 1";
+    
+        Game.Create(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT, Startup);
+        Game.Instance.Run();
+        Game.Instance.Dispose();
+        }
+
+        private static void Startup(object? sender, GameHost host)
+        {
+            Console startingConsole = Game.Instance.StartingConsole!;
+
+            startingConsole.Print(5, 5, "Welcome to RogueSharp using SadConsole !", Color.White);
+        }
+
+    }
+}
